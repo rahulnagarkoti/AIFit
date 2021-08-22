350f4c783bec6b5b82470bd4b1519c302a27b410
rahulnagarkoti <nagarkotirahul@hotmail.com>
2021-08-22 13:39:07 +1000

    initial architecture setup

---
+++ Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Build.Tasks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionRecommender.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [Column(TypeName= "varchar(50)")]
+        public string  Name { get; set; }
+        
+        [Required]
+        public DateTime DOB { get; set; }
+        
+        [Required]
+        public int Height { get; set; }
+
+        [Required]
+        public int Weight { get; set; }
+    }
+}
