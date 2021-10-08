0bf79615ba053a178ea92817e713a70fd902a514
rahulnagarkoti <nagarkotirahul@hotmail.com>
2021-10-08 12:11:44 +1100

    Rahul- minor design fixes

--- Controllers/SuggestionController.cs
+++ Controllers/SuggestionController.cs
@@ -13,8 +13,6 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace AIFit.Controllers
 {
 
